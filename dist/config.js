b8bcfd5dbdd6af3353bee936d91a80291322c910
Ludovic Anterieur <ludovic.anterieur@serviceonnewgrounds.com>
2023-02-09 15:31:07 +0800

    Add cmd argument for config file location Fixeds #7

--- dist/config.js
+++ dist/config.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadConfig = void 0;
+exports.getConfigPath = exports.loadConfig = void 0;
 const promises_1 = __importDefault(require("fs/promises"));
 const path_1 = __importDefault(require("path"));
 const Directory_1 = require("./Directory");
 async function loadConfig() {
-    const confPath = path_1.default.resolve(global.__dirname, `config.json`);
+    const confPath = getConfigPath();
     global.logger(`Loading config from ${confPath}.`);
     const config = JSON.parse(await promises_1.default.readFile(confPath, { encoding: `utf-8` }));
     if (config.exportDirectory) {
@@ -23,3 +23,17 @@ async function loadConfig() {
     return config;
 }
 exports.loadConfig = loadConfig;
+function getConfigPath() {
+    // check argv
+    if (process.argv.includes(`--config`)) {
+        const configDirPath = process.argv[process.argv.indexOf(`--config`) + 1].trim();
+        if (configDirPath?.length)
+            if (path_1.default.isAbsolute(configDirPath))
+                return configDirPath;
+            else
+                return path_1.default.resolve(global.__dirname, configDirPath);
+    }
+    // get default (executable folder)
+    return path_1.default.resolve(global.__dirname, `config.json`);
+}
+exports.getConfigPath = getConfigPath;
